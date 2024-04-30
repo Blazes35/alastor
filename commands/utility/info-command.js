c4ea6616cc1b07735d6f1a06430172016975ee32
Blazes35 <loridam2@gmail.com>
2024-04-30 16:51:08 +0200

    new info-command

--- commands/utility/info-command.js
+++ commands/utility/info-command.js
@@ -1,19 +1,30 @@
-
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    category: 'utility',
     data: new SlashCommandBuilder()
         .setName('info-command')
-        .setDescription('give informations about a command.')
+        .setDescription('Provides information about a specific command.')
         .addStringOption(option =>
             option.setName('command')
-                .setDescription('The command to get infos.')
+                .setDescription('The command to get information about.')
                 .setRequired(true)),
     async execute(interaction) {
         const commandName = interaction.options.getString('command', true).toLowerCase();
         const command = interaction.client.commands.get(commandName);
-        console.log(command.toString());
-        interaction.reply("```js"+command.toString()+"```");
+
+        if (!command) {
+            return interaction.reply(`There is no command with name \`${commandName}\`!`);
+        }
+
+        let reply = `**Command:** ${command.data.name}\n**Description:** ${command.data.description}`;
+
+        if (command.data.options) {
+            reply += '\n**Options:**\n';
+            for (const option of command.data.options) {
+                reply += `- ${option.name}: ${option.description} (required: ${option.required})\n`;
+            }
+        }
+
+        await interaction.reply(reply);
     },
 };
