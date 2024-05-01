e8ca61d793f08139051b7c3a3c121e7a91d6a8f0
Blazes35 <loridam2@gmail.com>
2024-05-01 17:23:03 +0200

    clean code and create embed presentation of the timeout

--- commands/utility/timeout.js
+++ commands/utility/timeout.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const {SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField, EmbedBuilder} = require('discord.js');
 const logChannels = require('../../logChannels.json');
 
 module.exports = {
@@ -6,44 +6,94 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('timeout')
         .setDescription('Times out a user.')
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to time out.')
-                .setRequired(true))
-        .addIntegerOption(option =>
-            option.setName('duration')
-                .setDescription('The duration of the timeout in seconds.')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('The reason for the timeout.')
-                .setRequired(true)),
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+        .addUserOption(option => option
+            .setName('user')
+            .setDescription('The user to time out.')
+            .setRequired(true))
+        .addIntegerOption(option => option
+            .setName('duration')
+            .setDescription('The duration of the timeout in seconds.')
+            .setRequired(true))
+        .addStringOption(option => option
+            .setName('reason')
+            .setDescription('The reason for the timeout.')
+            .setRequired(false)),
     async execute(interaction) {
-        await  interaction.deferReply({});
-        const user = interaction.options.getUser('user');
-        const duration = interaction.options.getInteger('duration')*1000;
-        const reason = interaction.options.getString('reason') || 'No reason provided';
-        const member = interaction.guild.members.cache.get(user.id);
-        if (!interaction.member.permissions.has(PermissionFlagsBits.ModerateMembers)) {
-            return interaction.editReply('You do not have permission to time out this user.');
-        }
-        if (!member) {
-            return interaction.editReply('User is not in this server.');
-        }
-        if (duration<5 || duration>2.419e9) {
-            return interaction.editReply('Duration is too long. Maximum is 28 days.');
-        }
-        try {
-            await interaction.editReply("Done :white_check_mark: ");
-            await member.timeout(duration); // convert minutes to milliseconds
+        await interaction.deferReply({ephemeral: true});
+        const {options, guild, user} = interaction;
 
+        const target = await options.getMember('user');
+        const duration = options.getInteger('duration') * 1000;
+        let reason = options.getString('reason') ?? 'No reason provided';
+
+        if (user.id === target.id) return interaction.editReply("You cannot time out yourself.");
+        if (!target.moderatable) return interaction.editReply("I can't time out this member.");
+        if (duration < 5 || duration > 2.419e9) return interaction.editReply('Duration must be between 5 seconds and 28 days.');
+
+        const senderRoles = interaction.member.roles.cache.map(r => r);
+        const targetPerms = target.permissions;
+
+        let higherPerms = new PermissionsBitField();
+
+        for (let role of senderRoles) {
+            if (role.rawPosition > target.roles.highest.rawPosition) {
+                higherPerms.add(role.permissions);
+            }
+        }
+
+        if (user.id !== guild.ownerId) {
+            if (!higherPerms.has('ModerateMembers'))
+                return interaction.editReply("You do not have permission to time out this user.");
+            else if (targetPerms.has('Administrator') && !higherPerms.has('Administrator'))
+                return interaction.editReply("You do not have permission to time out this user.");
+        }
+
+        let embed = new EmbedBuilder()
+            .setColor('ff0000')
+            .setAuthor({name: target.user.tag, iconURL: target.user.displayAvatarURL()})
+            .setTitle('New Infraction')
+            .setDescription(`Issued by ${user.tag}`)
+            .addFields(
+                {
+                    inline: true,
+                    name: 'Id:',
+                    value: "`" + target.user.id + "`"
+                },
+                {
+                    name: "Type:",
+                    value: "`Timeout`",
+                    inline: true
+                },
+                {
+                    name: "Guild:",
+                    value: "`" + guild.name + "`",
+                    inline: false
+                },
+                {
+                    name: "Duration:",
+                    value: "`" + duration + "`",
+                    inline: true
+                },
+                {
+                    name: "Reason:",
+                    value: "`" + reason + "`",
+                    inline: true
+                }
+            );
+
+
+
+
+        try{
+            await target.timeout(duration, reason);
+            await interaction.editReply({embeds: [embed]});
             // Get the log channel
             const logChannel = interaction.guild.channels.cache.get(logChannels[interaction.guild.id.toString()]);
-            // Send a message to the log channel
-            logChannel.send(`User ${user.tag} has been timed out for ${duration/1000} seconds. Reason: ${reason}`);
-        } catch (error) {
+            logChannel.send({embeds:[embed]});
+        }catch (error) {
             console.error(error);
-            await interaction.editReply( 'There was an error while trying to time out this user!');
+            await interaction.editReply('There was an error while trying to time out this user!');
         }
-    },
+    }
 };
