c71d1d8c40397325df8c52a6fe4c65ac4fb8d69f
Blazes35 <loridam2@gmail.com>
2024-06-01 23:13:45 +0200

    Add buttons and delay: -confirmation -cancell -delay auto-cancell

--- commands/utility/timeout.js
+++ commands/utility/timeout.js
@@ -1,7 +1,15 @@
 // noinspection JSUnresolvedReference,JSUnusedLocalSymbols
 
-const {SlashCommandBuilder, PermissionsBitField, EmbedBuilder, default_member_permissions } = require('discord.js');
-const { PermissionFlagsBits } = require('discord-api-types/v10');
+const {
+    SlashCommandBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    PermissionsBitField,
+    EmbedBuilder,
+    default_member_permissions
+} = require('discord.js');
+const {PermissionFlagsBits} = require('discord-api-types/v10');
 const logChannels = require('../../logChannels.json');
 const autoEmbedBuilder = require('../../events/autoEmbedBuilder').createInfractionEmbed;
 
@@ -30,23 +38,23 @@ module.exports = {
         const target = await options.getMember('user');
         let durationOption = options.getString('duration').split(' ');
         let reason = options.getString('reason') ?? 'No reason provided';
-        let duration=0;
+        let duration = 0;
 
         for (const time of durationOption) {
             if (/(\d+)[wW]/.test(time)) {
-                duration += parseInt(time) *7*24*60*60;
+                duration += parseInt(time) * 7 * 24 * 60 * 60;
             } else if (/(\d+)[dD]/.test(time)) {
-                duration += parseInt(time) *24*60*60;
+                duration += parseInt(time) * 24 * 60 * 60;
             } else if (/(\d+)[hH]/.test(time)) {
-                duration += parseInt(time) *60*60;
+                duration += parseInt(time) * 60 * 60;
             } else if (/(\d+)[mM]/.test(time)) {
-                duration += parseInt(time) *60;
+                duration += parseInt(time) * 60;
             } else if (/(\d+)[sS]/.test(time)) {
                 duration += parseInt(time);
             } else if (/(\d+)/.test(time) && durationOption.length === 1) {
                 duration = parseInt(time);
                 durationOption[0] = durationOption[0] + 's';
-            }else {
+            } else {
                 return interaction.editReply('Invalid duration format.');
             }
         }
@@ -66,6 +74,9 @@ module.exports = {
             }
         }
 
+        // noinspection JSCheckFunctionSignatures
+        let embed = autoEmbedBuilder(target, user, 'timeout', guild, durationOption, reason);
+
         if (user.id !== guild.ownerId) {
             if (!higherPerms.has('ModerateMembers'))
                 return interaction.editReply("You do not have permission to time out this user.");
@@ -73,19 +84,55 @@ module.exports = {
                 return interaction.editReply("You do not have permission to time out this user.");
         }
 
-        // noinspection JSCheckFunctionSignatures
-        let embed = autoEmbedBuilder(target, user, 'timeout',guild, durationOption, reason);
+        const confirm = new ButtonBuilder()
+            .setCustomId('confirm')
+            .setLabel('Confirm timeout')
+            .setStyle(ButtonStyle.Danger);
 
-        try{
-            duration*=1000;
-            await target.timeout(duration, reason);
-            await interaction.editReply({silent:true, embeds: [embed]});
-            // Get the log channel
-            const logChannel = interaction.guild.channels.cache.get(logChannels[interaction.guild.id.toString()]);
-            logChannel.send({silent:true, embeds:[embed]});
-        }catch (error) {
-            console.error(error);
-            await interaction.editReply('There was an error while trying to time out this user!');
+        const cancel = new ButtonBuilder()
+            .setCustomId('cancel')
+            .setLabel('Cancel')
+            .setStyle(ButtonStyle.Secondary);
+
+        const row = new ActionRowBuilder()
+            .addComponents(cancel, confirm);
+
+        const response = await interaction.editReply({
+            silent: true,
+            content: `Are you sure you want to ban ${target} for reason: ${reason}?`,
+            components: [row],
+        });
+
+        const collectorFilter = i => i.user.id === interaction.user.id;
+
+        try {
+            const confirmation = await response.awaitMessageComponent({filter: collectorFilter, time: 60_000});
+
+            if (confirmation.customId === 'confirm') {
+
+                duration *= 1000;
+                await target.timeout(duration, reason);
+                await interaction.editReply({silent: true, embeds: [embed], components: []});
+
+
+                // Get the log channel and set the embed recap
+                const logChannel = interaction.guild.channels.cache.get(logChannels[interaction.guild.id.toString()]);
+                logChannel.send({silent: true, embeds: [embed]});
+
+            } else if (confirmation.customId === 'cancel') {
+                await confirmation.update({content: 'Action cancelled', components: []});
+            }
+        } catch (e) {
+            if (e.message === 'Collector received no interactions before ending with reason: time') {
+                // Send auto-cancel message if there is a delay error
+                await interaction.editReply({
+                    content: 'Confirmation not received within 1 minute, cancelling',
+                    components: []
+                });
+            } else {
+                // Send all other types of errors to the console
+                console.error(e.message);
+            }
         }
     }
 };
